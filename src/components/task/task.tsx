02503620fca5b4a5d07d888e03931a6a2f9cbdbe
sotatek-HarryPhi <e.hai.phi@wesang.com>
2022-10-16 22:29:22 +0700

    20. Starting with the Task Component

---
+++ src/components/task/task.tsx
@@ -0,0 +1,27 @@
+import { Box } from '@mui/material';
+import { borderRadius } from '@mui/system';
+import React, { FC, ReactElement } from 'react';
+
+const Task: FC = (props): ReactElement => {
+  return (
+    <Box
+      display="flex"
+      width="100%"
+      justifyContent="flex-start"
+      flexDirection="column"
+      mb={2}
+      p={4}
+      sx={{
+        width: '100%',
+        backgroundColor: 'background.paper',
+        borderRadius: '8px',
+        border: '1px solid',
+        borderColor: 'error.light',
+      }}
+    >
+      Task
+    </Box>
+  );
+};
+
+export default Task;
--- src/components/taskArea/taskArea.tsx
+++ src/components/taskArea/taskArea.tsx
@@ -1,6 +1,7 @@
 import { Box, Grid } from '@mui/material';
 import { format } from 'date-fns';
 import React, { FC, ReactElement } from 'react';
+import Task from '../task/task';
 import TaskCounter from '../taskCounter/taskCounter';
 
 export const TaskArea: FC = (): ReactElement => {
@@ -38,8 +39,9 @@ export const TaskArea: FC = (): ReactElement => {
           xs={10}
           md={8}
         >
-          <Box>Tasks Will Come Over Here</Box>
-          <Box>Tasks Will Come Over Here</Box>
+          <Task />
+          <Task />
+          <Task />
         </Grid>
       </Grid>
     </Grid>
