e47c9df27312bc29c0fa0edb755888435fd792ec
sotatek-HarryPhi <e.hai.phi@wesang.com>
2022-10-16 22:04:42 +0700

    19. Emitting Correct Label

--- src/components/taskCounter/taskCounter.tsx
+++ src/components/taskCounter/taskCounter.tsx
@@ -1,7 +1,9 @@
 import { Avatar, Box, Typography } from '@mui/material';
+import PropTypes from 'prop-types';
 import React, { FC, ReactElement } from 'react';
 import { Status } from '../createTaskForm/enum/Status';
 import { emitCorrectBorderColor } from './helpers/emitCorrectBorderColor';
+import { emitCorrectLabel } from './helpers/emitCorrectLabel';
 import { ITaskCounter } from './interfaces/ITaskCounter';
 
 const TaskCounter: FC<ITaskCounter> = (
@@ -26,7 +28,7 @@ const TaskCounter: FC<ITaskCounter> = (
         }}
       >
         <Typography color="#ffffff" variant="h4">
-          10
+          {count}
         </Typography>
       </Avatar>
       <Typography
@@ -35,10 +37,19 @@ const TaskCounter: FC<ITaskCounter> = (
         fontSize="20px"
         variant="h5"
       >
-        Subtitle
+        {emitCorrectLabel(status)}
       </Typography>
     </Box>
   );
+};
+
+TaskCounter.propTypes = {
+  count: PropTypes.number,
+  status: PropTypes.oneOf([
+    Status.todo,
+    Status.inProgress,
+    Status.completed,
+  ]),
 };
 
 export default TaskCounter;
